a789b1b1f0d602d86e1709a4666fae132bcc8db5
Rajendaran <rajendren@gmail.com>
2017-07-14 11:57:40 -0700

    Initial Commit

---
+++ KaHaCove/CloveWearable/src/main/java/com/clovewearable/cove/CloveWearableApplication.java
@@ -0,0 +1,12 @@
+package com.clovewearable.cove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloveWearableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloveWearableApplication.class, args);
+	}
+}
